4ce82294a3f64f7d25fccf68b6955570843e31d8
Arvand Molla <mr.arvand@gmail.com>
2021-05-10 14:57:21 +0200

    1

--- linkedin/src/App.js
+++ linkedin/src/App.js
@@ -1,17 +1,13 @@
-
 import { Switch, Router, Route } from "react-router-dom";
-import './App.css';
-import MyNavbar from './components/MyNavbar';
-import logo from "./logo.svg";
+import "./App.css";
+import MyNavbar from "./components/MyNavbar";
 
 import "./App.css";
 import Profile from "./components/Profile.jsx";
 
-
 function App() {
   return (
     <div className="App">
-      <MyNavbar />
       <h1>Hello!</h1>
     </div>
   );
