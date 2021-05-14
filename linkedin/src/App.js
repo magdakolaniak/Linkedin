6df3a0a49036ce52c2405bd48ce04c8f65abb39a
Arvand Molla <mr.arvand@gmail.com>
2021-05-14 15:48:28 +0200

    Update App.js

--- linkedin/src/App.js
+++ linkedin/src/App.js
@@ -1,24 +1,20 @@
-
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
 import "./index.css";
 import Profile from "./components/Profile.jsx";
-import React, { useReducer } from "react";
 import MyNavbar from "./components/MyNavbar";
 import Feeds from "./components/Feeds";
-import React from 'react';
-import MyFooter from './components/MyFooter';
-import Home from './components/Home';
-
+import MyFooter from "./components/MyFooter";
+import Home from "./components/Home";
 
 function App() {
   return (
     <div>
       <Router>
         <MyNavbar />
-        <Route component={Home} path="/home" />
+        <Route component={Home} path="/home" exact />
         <Route component={Profile} path="/userprofile/:userId" />
-        
+
         <MyFooter />
       </Router>
     </div>
