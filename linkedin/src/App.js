3cd741f135aa6b3deb2b80c05dc802f4e180e1a4
Arvand Molla <mr.arvand@gmail.com>
2021-05-14 10:09:19 +0200

    changing routes

--- linkedin/src/App.js
+++ linkedin/src/App.js
@@ -1,19 +1,21 @@
-import MyFooter from './components/MyFooter';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import './App.css';
+import MyFooter from "./components/MyFooter";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import "./App.css";
 
-import './index.css';
-import Profile from './components/Profile.jsx';
+import "./index.css";
+import Profile from "./components/Profile.jsx";
 
-import React from 'react';
-import MyNavbar from './components/MyNavbar';
-import Feeds from './components/Feeds';
+import React, { useReducer } from "react";
+import MyNavbar from "./components/MyNavbar";
+import Feeds from "./components/Feeds";
 
 function App() {
   return (
     <div>
       <Router>
-        <Feeds />
+        <Route component={Feeds} path="/" exact />
+
+        <Route component={Profile} path="/userprofile/:userId" />
       </Router>
     </div>
   );
